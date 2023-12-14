73cb1d26f1ebd3d160dbd13883f7d711fc4759c9
Roman Gudz <alien31roman@gmail.com>
2023-12-15 00:08:20 +0300

    fix error

--- script/modal.js
+++ script/modal.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const getModalInput = (value) => document.getElementsByName(value);
-
-const modalName = getModalInput('name');
-const modalCategory = getModalInput('category');
-const modalDiscount = getModalInput('discount');
-const modalDiscountInput = getModalInput('discount_count');
+const modalName = document.querySelector('#name');
+const modalCategory = document.querySelectorAll('.modal__form');
+const modalDiscount = document.querySelector('#discount');
+const modalDiscountInput = document.querySelector('.modal__input_discount');
